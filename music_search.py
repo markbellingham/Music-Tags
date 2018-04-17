668d6b4dd493d7988c0838591d6c1885a6d66c34
Mark Bellingham <mark.bellingham@protonmail.ch>
2018-04-17 15:06:52 +0100

    program supports ogg vorbis files

--- music_search.py
+++ music_search.py
@@ -3,6 +3,7 @@
 import os
 from mutagen.flac import FLAC
 from mutagen.mp3 import MP3
+from mutagen.oggvorbis import OggVorbis
 import music_tag_list_mp3 as mtl_mp3
 import music_tag_list_flac as mtl_flac
 
@@ -24,7 +25,6 @@ def main():
         for filename in files:
             filename = os.path.join(root, filename)
             if filename.endswith('.mp3'):
-                print("mp3 - " + filename)
                 try:
                     audio = MP3(filename)
                     mtl_mp3.artist(audio, filename) or \
@@ -44,6 +44,20 @@ def main():
                 try:
                     audio = FLAC(filename)
                     mtl_flac.artist(audio, filename) or \
+                        mtl_flac.album(audio, filename) or \
+                        mtl_flac.album_artist(audio, filename) or \
+                        mtl_flac.trackna(audio, filename) or \
+                        mtl_flac.trackno(audio, filename) or \
+                        mtl_flac.genre(audio, filename) or \
+                        mtl_flac.duration(audio, filename)
+                except Exception as error:
+                    print("error - ", error)
+                finally:
+                    print()
+            elif filename.endswith('.ogg'):
+                try:
+                    audio = OggVorbis(filename)
+                    mtl_flac.album(audio, filename) or \
                         mtl_flac.album(audio, filename) or \
                         mtl_flac.album_artist(audio, filename) or \
                         mtl_flac.trackna(audio, filename) or \
